af8c9a7e682efc253a74ef5156c4d186e516ccf3
pathumlakshan1101 <lakshanherath1101@gmail.com>
2022-08-16 06:53:06 +0530

    first commit

---
+++ src/fxApplications/SignApp.java
@@ -0,0 +1,15 @@
+package fxApplications;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SignApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
